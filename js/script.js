85ed42507cda7f70c047c4b05fcd0fdc232aeb5f
rlee38 <rlee38@uco.edu>
2020-10-25 00:20:48 -0500

    happy saturday

--- js/script.js
+++ js/script.js
@@ -20,3 +20,52 @@ function search() {
         alert("type something la if u want to search lnao")
     }
 }
+
+// let playing = false;
+// function music() {
+//     sound = document.getElementById('sound1');
+//     if (!playing) {
+//         sound.play();
+//         document.getElementById('soundbutton1').innerHTML = "Stop it"
+//         document.getElementById('soundbutton2').innerHTML = "Stop it"
+//     } else {
+//         sound.pause();
+//         sound.currentTime = 0;
+//         document.getElementById('soundbutton1').innerHTML = "Play it"
+//         document.getElementById('soundbutton2').innerHTML = "Play it"
+//     }
+//     playing = !playing
+// }
+
+// function goblin() {
+//     sound = document.getElementById('goblin')
+//     sound.play()
+// }
+
+// let rickrollplaying = false;
+// function rickroll() {
+//     sound = document.getElementById('rickroll');
+//     if (!rickrollplaying) {
+//         sound.play();
+//         document.getElementById('rickrollbutton').innerHTML = "Pause"
+//     } else {
+//         sound.pause();
+//         document.getElementById('rickrollbutton').innerHTML = "Play it"
+//     }
+//     rickrollplaying = !rickrollplaying
+// }
+
+function music(audio, buttons) {
+    sound = document.getElementById(audio);
+    if (sound.paused) {
+        sound.play();
+        buttons.forEach(element => {
+            document.getElementById(element).innerHTML = "Pause"
+        });
+    } else {
+        sound.pause();
+        buttons.forEach(element => {
+            document.getElementById(element).innerHTML = "Play it"
+        });
+    }
+}
